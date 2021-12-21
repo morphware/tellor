815cb318d09e290bb30010d51bca97f35ae0da75
Lamont V <lxveal1983@gmail.com>
2021-12-20 19:12:43 -0500

    Update endpoint.sol

--- contracts/endpoint.sol
+++ contracts/endpoint.sol
@@ -1,1 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.3;
 
+import "./ITellor.sol";
+import "./SafeMath.sol";
+import "./UsingTellor.sol";
+import "./TellorPlayground.sol";
+
+contract endPoint is UsingTellor {
+	using SafeMath for uint256;
+
+	// endpoint
+	bytes32 public _endpoint;
+	uint256 public value;
+
+    constructor(address payable _tellor) {
+        tellor = ITellor(_tellor);
+    }
+
+    function getEndPoint(bytes32 _endpoint) returns (bytes32) {
+        return _endpoint;
+    }
+    
+    function getDataBefore(uint256 _requestId, uint256 _timestamp)
+        public
+        view
+        returns (
+            bool _ifRetrieve,
+            uint256 _value,
+            uint256 _timestampRetrieved
+        )
+    {
+        (bool _found, uint256 _index) =
+            getIndexForDataBefore(_requestId, _timestamp);
+        if (!_found) return (false, 0, 0);
+        uint256 _time =
+            tellor.getTimestampbyRequestIDandIndex(_requestId, _index);
+        _value = tellor.retrieveData(_requestId, _time);
+        //If value is diputed it'll return zero
+        if (_value > 0) return (true, _value, _time);
+        return (false, 0, 0);
+      }
+
+    }
